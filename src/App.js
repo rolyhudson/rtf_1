829445769957154374e16692dd19bce5bbe4a6d9
rhudson <rhudson@hksinc.com>
2023-01-24 16:03:08 +0000

    Set up rdf version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { coords } from "./capMarkers";
+import { OrbitControls } from "@react-three/drei";
+import {
+  BufferAttribute,
+  BufferGeometry,
+  Float32BufferAttribute,
+  Matrix4,
+} from "three";
 
-function App() {
+export default function App() {
+  const bufferGeo = new BufferGeometry();
+  bufferGeo.setAttribute("position", new Float32BufferAttribute(coords, 3));
+
+  bufferGeo.computeBoundingSphere();
+  let center = bufferGeo.boundingSphere.center;
+  bufferGeo.applyMatrix4(new Matrix4().makeRotationX(-Math.PI / 2));
+  bufferGeo.translate(-center.x, -center.y, -center.z);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas
+        camera={{
+          fov: 35,
+          position: [35438, 0, 35438],
+          far: 71000,
+          near: 1,
+          aspect: window.innerWidth / window.innerHeight,
+        }}
+      >
+        <lineSegments visible geometry={bufferGeo}>
+          <lineBasicMaterial attach="material" color="black" />
+        </lineSegments>
+
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
-
-export default App;
